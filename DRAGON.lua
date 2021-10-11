e2d07b5b406d2296a8550822aba43e744e1d9813
Gambol00 <92166459+Gambol00@users.noreply.github.com>
2021-10-11 21:08:38 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3904,7 +3904,7 @@ keyboard.inline_keyboard = {
 {{text = 'ᏴᎬΝY💕',url="t.me/S_79_Q"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/S_79_Q&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/SEMO87811&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 if text == 'شهاب' or text == 'قرصان' or text == 'قرصان' then
 local Text = [[
